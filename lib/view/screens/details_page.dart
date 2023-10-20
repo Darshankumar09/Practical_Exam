b0c6aabc1a8d7e42a0bf42f5f2be5841f25842e0
Darshankumar09 <darshansutariya2003@gmail.com>
2023-10-20 17:10:17 +0530

    20/10/2023

--- lib/view/screens/details_page.dart
+++ lib/view/screens/details_page.dart
@@ -18,6 +18,54 @@ class _DetailsPageState extends State<DetailsPage> {
       appBar: AppBar(
         title: const Text("Details Page"),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                data.imageUrl!,
+                height: 400,
+                width: 200,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Id : ${data.id}",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Name : ${data.name}",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Description :",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              data.description!,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
